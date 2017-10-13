c28bc69357f2037a118f4577394b261b33f553e9
Hao Xu <xuh@email.unc.edu>
2017-10-13 18:43:34 -0400

    refactor checkschema

--- checkschema.sql
+++ checkschema.sql
@@ -1,27 +1,23 @@
 create or replace function table_exists(table_name text) returns boolean as $$
   declare
-    ret boolean;
   begin
-    execute E'select EXISTS (
+    return EXISTS (
       SELECT 1
       FROM   pg_catalog.pg_class c
       JOIN   pg_catalog.pg_namespace n ON n.oid = c.relnamespace
       WHERE  n.nspname = ANY(current_schemas(FALSE))
-      AND    lower(c.relname) similar to lower($1)
-      AND    c.relkind = \'r\')' into ret using table_name;
-    return ret;
+      AND    lower(c.relname) similar to lower(table_name)
+      AND    c.relkind = 'r');
   end
 $$ language plpgsql;
 
-create or replace function column_exists(table_name text, column_name text) returns boolean as $$
+create or replace function column_exists(table_npat text, column_npat text) returns boolean as $$
   declare
-    ret boolean;
   begin
-   execute 'select EXISTS (
+   return EXISTS (
       SELECT column_name 
-      FROM information_schema.columns 
-      WHERE lower(table_name) similar to lower($1) and lower(column_name) similar to lower($2))' into ret using table_name, column_name;
-   return ret;
+      FROM information_schema.columns
+      WHERE lower(table_name) similar to lower(table_npat) and lower(column_name) similar to lower(column_npat));
   end
 $$ language plpgsql;
 
@@ -57,12 +53,14 @@ create or replace function check_table_not_exists(table_name text) returns void 
   end
 $$ language plpgsql;
 
-select check_table_exists('cmaq\_7da\_DES', array['col','row','date','DESpm','DESo']);
-select check_table_exists('observation\_fact', array['patient\_num','encounter\_num','concept\_cd','nval_num','start\_date']);
-select check_table_exists('patient\_dimension', array['patient\_num', 'birth\_date', 'sex\_cd', 'race\_cd']);
-select check_table_exists('visit\_dimension', array['patient\_num', 'encounter\_num', 'inout\_cd']);
-select check_table_not_exists('%reduced%');
-select check_table_not_exists('%asthma%');
-select check_table_not_exists('features');
+do $$ begin
+  select check_table_exists('cmaq\_7da\_DES', array['col','row','date','DESpm','DESo']);
+  select check_table_exists('observation\_fact', array['patient\_num','encounter\_num','concept\_cd','nval_num','start\_date']);
+  select check_table_exists('patient\_dimension', array['patient\_num', 'birth\_date', 'sex\_cd', 'race\_cd']);
+  select check_table_exists('visit\_dimension', array['patient\_num', 'encounter\_num', 'inout\_cd']);
+  select check_table_not_exists('%reduced%');
+  select check_table_not_exists('%asthma%');
+  select check_table_not_exists('features');
+end $$;
 
 
