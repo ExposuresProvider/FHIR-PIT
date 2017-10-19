51f59f86a61a4b3df8151f31229e94e98d9ad105
Hao Xu <xuh@cs.unc.edu>
2017-10-19 17:45:23 -0400

    refactor

--- boxplot.r
+++ boxplot.r
@@ -2,6 +2,8 @@ library(data.table)
 
 df <- fread("/tmp/boxplot.csv")
 
-boxplot(despm_7da~visit_type,data=df)
-boxplot(deso_7da~visit_type,data=df)
+boxplot(despm_7da~visit_type,data=df, xlab="visit_type", ylab="despm_7da")
+
+dev.copy(png, "pm25_7da_visit_type.png", width=800, height = 600)
+dev.off()
 
