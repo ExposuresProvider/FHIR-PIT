2a0dc3c11bd30983ec43f338c6ab9eae6098b2bc
Hao Xu <xuh@cs.unc.edu>
2018-01-09 18:14:54 -0500

    run ddot

--- cooccurrence/python/onto.py
+++ cooccurrence/python/onto.py
@@ -6,6 +6,8 @@ import numpy as np
 import igraph
 import sys
 
+sys.path.insert(0, '/home/a/ontology')
+
 # import seaborn as sns
 import ddot
 from ddot import Ontology, nx_to_NdexGraph, read_term_descriptions, ndex_to_sim_matrix, expand_seed, melt_square, align_hierarchies, parse_ndex_uuid
@@ -16,6 +18,7 @@ beta = float(sys.argv[2])
 singleton = sys.argv[3]
 cooc = sys.argv[4]
 output = sys.argv[5]
+upload = sys.argv[6] == "true"
 
 singleton_df = pd.read_csv(singleton,sep=" ",header=None)
 cooc_df = pd.read_csv(cooc,sep=" ", header = None)
@@ -29,8 +32,8 @@ print(frequency_dict)
 def normalize(x):
     concept1 = x[0]
     concept2 = x[1]
-    x[0] = x[0].replace(':', '_')
-    x[1] = x[1].replace(':', '_')
+    x[0] = x[0].replace(' ', '_')
+    x[1] = x[1].replace(' ', '_')
     x[2] = float(x[2])/(frequency_dict[concept1] * frequency_dict[concept2])
 #    print(x)
     return x
@@ -49,3 +52,12 @@ print(ont.to_table(output, clixo_format=True))
 
 grph = ont.to_igraph(include_genes=True)
 grph.write_graphml(output+'.graphml')
+
+if upload:
+    ndex_url, ont_ndexgraph = ont.to_ndex(name="Pitch 15 Asthma Ontology",
+                                          ndex_server="http://test.ndexbio.org",
+                                          ndex_pass='scratch',
+                                          ndex_user='scratch',
+                                          layout='bubble-collect',
+                                          visibility='PUBLIC') 
+    print ndex_url
