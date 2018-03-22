07aa43af45cfa470047ce0b152be35e0f858ebaa
Hao Xu <xuh@email.unc.edu>
2018-03-22 13:13:23 -0400

    preproc cmaq 2011

--- spark/src/main/python/preproc_cmaq_2011.py
+++ spark/src/main/python/preproc_cmaq_2011.py
@@ -6,7 +6,7 @@ import sys
 input_dir = sys.argv[1]
 output_dir = sys.argv[2]
 
-for filename in glob.glob(os.path.join(input_dir, "C*")):
+for filename in glob.glob(os.path.join(input_dir, "C*", "*.csv")):
     print("processing", filename)
     shutil.move(filename, os.path.join(output_dir, os.path.basename(filename).replace("_extractions_12k", "")))
 
