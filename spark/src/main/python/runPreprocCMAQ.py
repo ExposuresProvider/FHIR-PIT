13a9e7c79dd2fe50d8ef41b71929f0cf722a6f9e
Hao Xu <xuh@email.unc.edu>
2018-03-22 14:26:11 -0400

    preproc cmaq

---
+++ spark/src/main/python/runPreprocCMAQ.py
@@ -0,0 +1,29 @@
+import os
+import sys
+import subprocess
+from timeit import default_timer as timer
+import sys
+
+dir = sys.argv[1]
+cache_dir = sys.argv[3]
+
+start = timer()
+cmd = ["spark-submit",
+       "--master",
+       "spark://a-HP-Z820-Workstation:7077",
+       "--jars",
+       cache_dir + "/.ivy2/cache/com.github.scopt/scopt_2.11/jars/scopt_2.11-3.7.0.jar," +
+       cache_dir + "/.ivy2/cache/org.locationtech.geotrellis/geotrellis-proj4_2.11/jars/geotrellis-proj4_2.11-1.1.0.jar",
+       "--class",
+       "datatrans.PreprocCMAQ",
+       "target/scala-2.11/preproc_2.11-1.0.jar",
+       dir + "/cmaq/cmaq2010.csv",
+       dir + "/cmaq2010"]
+proc = subprocess.Popen(cmd)
+err = proc.wait()
+if err:
+    print("error:", err)
+end = timer()
+print(end - start)
+
+
