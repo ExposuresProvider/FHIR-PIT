84baf26861471f0f9282fc748ac23bbd153f24c5
Hao Xu <xuh@email.unc.edu>
2018-03-22 14:33:39 -0400

    preproc cmaq

--- spark/src/main/python/runChunkPerPat.py
+++ spark/src/main/python/runChunkPerPat.py
@@ -6,11 +6,12 @@ import sys
 
 dir = sys.argv[1]
 cache_dir = sys.argv[2]
+host_name = sys.argv[3]
 
 start = timer()
 cmd = ["spark-submit",
        "--master",
-       "spark://a-HP-Z820-Workstation:7077",
+       "spark://{0}:7077".format(host_name),
        "--jars",
        cache_dir + "/.ivy2/cache/com.github.scopt/scopt_2.11/jars/scopt_2.11-3.7.0.jar," +
        cache_dir + "/.ivy2/cache/org.locationtech.geotrellis/geotrellis-proj4_2.11/jars/geotrellis-proj4_2.11-1.1.0.jar",
@@ -19,7 +20,7 @@ cmd = ["spark-submit",
        "target/scala-2.11/preproc_2.11-1.0.jar",
        dir + "/patient_dimension.csv",
        dir + "/visit_dimension.csv",
-       dir + "/observation_fact.csv"
+       dir + "/observation_fact.csv",
        dir + "/patient_series/"]
 proc = subprocess.Popen(cmd)
 err = proc.wait()
