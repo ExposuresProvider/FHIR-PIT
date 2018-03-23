964e95711d5d3df628c2842c62c4df975e7717f7
Hao Xu <xuh@email.unc.edu>
2018-03-23 14:59:58 -0400

    skip if nonexistent patient visit_dimension or observation_fact file

--- spark/src/main/scala/datatrans/PreprocPerPatSeries.scala
+++ spark/src/main/scala/datatrans/PreprocPerPatSeries.scala
@@ -46,70 +46,67 @@ object PreprocPerPatSeries {
               val vdif = config.input_directory + "/visit_dimension/patient_num=" + p + ".csv"
               val ofif = config.input_directory + "/observation_fact/patient_num=" + p + ".csv"
 
-              println("loading patient_dimension from " + pdif)
-              val pddf = spark.read.format("csv").option("header", true).load(pdif)
-              println("loading observation_fact from " + ofif)
-              val ofdf = spark.read.format("csv").option("header", true).load(ofif)
-
-              val pat = pddf.select("race_cd", "sex_cd", "birth_date")
-
-              val lat = ofdf.filter($"concept_cd".like("GEO:LAT")).select("nval_num").agg(avg("nval_num").as("lat"))
-
-              val lon = ofdf.filter($"concept_cd".like("GEO:LONG")).select("nval_num").agg(avg("nval_num").as("lon"))
-
-              val features = pat
-                .crossJoin(lat)
-                .crossJoin(lon)
-
-              // observation
-              val observation_cols = Seq(
-                "valueflag_cd",
-                "valtype_cd",
-                "nval_num",
-                "tval_char",
-                "units_cd",
-                "start_date",
-                "end_date"
-              )
-              val observation = ofdf.select("encounter_num", "concept_cd", "instance_num", "modifier_cd", "valueflag_cd", "valtype_cd", "nval_num", "tval_char", "units_cd", "start_date", "end_date").orderBy("start_date")
-
-              val observation_wide = aggregate(observation, Seq("encounter_num", "concept_cd", "instance_num", "modifier_cd"), observation_cols, "observation")
-
-              // visit
-              println("loading visit_dimension from " + vdif)
               val vdif_path = new Path(vdif)
               val vdif_fs = vdif_path.getFileSystem(hc)
-              val vddf = if(vdif_fs.exists(vdif_path)) {
-                spark.read.format("csv").option("header", true).load(vdif)
+              val ofif_path = new Path(ofif)
+              val ofif_fs = ofif_path.getFileSystem(hc)
+
+              if(!vdif_fs.exists(vdif_path) || !ofif_fs.exists(ofif_path)) {
+                println("of or vd file not found, skipped " + p)
               } else {
-                val schema = StructType(Seq(
-                  StructField("inout_cd", StringType, true),
-                  StructField("start_date", StringType, true),
-                  StructField("end_date", StringType, true)
-                ))
+                println("loading patient_dimension from " + pdif)
+                val pddf = spark.read.format("csv").option("header", true).load(pdif)
+                println("loading observation_fact from " + ofif)
+                val ofdf = spark.read.format("csv").option("header", true).load(ofif)
+                println("loading visit_dimension from " + vdif)
+                val vddf = spark.read.format("csv").option("header", true).load(vdif)
 
-                spark.createDataFrame(spark.sparkContext.emptyRDD[Row], schema)
+                val pat = pddf.select("race_cd", "sex_cd", "birth_date")
+
+                val lat = ofdf.filter($"concept_cd".like("GEO:LAT")).select("nval_num").agg(avg("nval_num").as("lat"))
+
+                val lon = ofdf.filter($"concept_cd".like("GEO:LONG")).select("nval_num").agg(avg("nval_num").as("lon"))
+
+                val features = pat
+                  .crossJoin(lat)
+                  .crossJoin(lon)
+
+                // observation
+                val observation_cols = Seq(
+                  "valueflag_cd",
+                  "valtype_cd",
+                  "nval_num",
+                  "tval_char",
+                  "units_cd",
+                  "start_date",
+                  "end_date"
+                )
+                val observation = ofdf.select("encounter_num", "concept_cd", "instance_num", "modifier_cd", "valueflag_cd", "valtype_cd", "nval_num", "tval_char", "units_cd", "start_date", "end_date").orderBy("start_date")
+
+                val observation_wide = aggregate(observation, Seq("encounter_num", "concept_cd", "instance_num", "modifier_cd"), observation_cols, "observation")
+
+                // visit
+                val visit_cols = Seq(
+                  "inout_cd",
+                  "start_date",
+                  "end_date"
+                )
+                val visit = vddf.select("encounter_num", "inout_cd", "start_date", "end_date").orderBy("start_date")
+
+                val visit_wide = aggregate(visit, Seq("encounter_num"), visit_cols, "visit")
+
+                //      val merge_map = udf((a:Map[String,Any], b:Map[String,Any]) => mergeMap(a,b))
+
+                val features_wide = features
+                    .crossJoin(observation_wide)
+                    .crossJoin(visit_wide)
+                //        .select($"patient_num", $"encounter_num", $"sex_cd", $"race_cd", $"birth_date", merge_map($"observation", $"visit"))
+
+                // https://stackoverflow.com/questions/41601874/how-to-convert-row-to-json-in-spark-2-scala
+                val json = features_wide.toJSON.first()
+
+                writeToFile(hc, config.output_prefix + p, json)
               }
-              val visit_cols = Seq(
-                "inout_cd",
-                "start_date",
-                "end_date"
-              )
-              val visit = vddf.select("encounter_num", "inout_cd", "start_date", "end_date").orderBy("start_date")
-
-              val visit_wide = aggregate(visit, Seq("encounter_num"), visit_cols, "visit")
-
-              //      val merge_map = udf((a:Map[String,Any], b:Map[String,Any]) => mergeMap(a,b))
-
-              val features_wide = features
-                  .crossJoin(observation_wide)
-                  .crossJoin(visit_wide)
-              //        .select($"patient_num", $"encounter_num", $"sex_cd", $"race_cd", $"birth_date", merge_map($"observation", $"visit"))
-
-              // https://stackoverflow.com/questions/41601874/how-to-convert-row-to-json-in-spark-2-scala
-              val json = features_wide.toJSON.first()
-
-              writeToFile(hc, config.output_prefix + p, json)
 
             }
 
