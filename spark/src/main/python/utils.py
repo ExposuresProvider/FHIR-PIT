48199dbe2c787b6004d3c7c4f7aba6935f376172
Hao Xu <xuh@email.unc.edu>
2018-03-25 03:41:54 -0400

    preproc geodata

--- spark/src/main/python/utils.py
+++ spark/src/main/python/utils.py
@@ -15,7 +15,7 @@ def submit(host_name, cache_dir, cls, *args, **kwargs):
            cache_dir + "/.ivy2/cache/org.locationtech.geotrellis/geotrellis-proj4_2.11/jars/geotrellis-proj4_2.11-1.1.0.jar",
            "--class",
            cls,
-           "target/scala-2.11/preproc_2.11-1.0.jar"] + args
+           "target/scala-2.11/preproc_2.11-1.0.jar"] + list(args)
     if "log" in kwargs and "log2" in kwargs:
         log = kwargs["log"]
         log2 = kwargs["log2"]
