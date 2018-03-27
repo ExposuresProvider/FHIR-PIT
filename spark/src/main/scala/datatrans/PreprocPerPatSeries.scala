b65f99f75b15ac380d9069dfb650b80e2277eb80
Hao Xu <xuh@cs.unc.edu>
2018-03-27 16:36:02 -0400

    skip existing files

--- spark/src/main/scala/datatrans/PreprocPerPatSeries.scala
+++ spark/src/main/scala/datatrans/PreprocPerPatSeries.scala
@@ -42,76 +42,84 @@ object PreprocPerPatSeries {
             time {
 
               val hc = spark.sparkContext.hadoopConfiguration
-
-              val pdif = config.input_directory + "/patient_dimension/patient_num=" + p + ".csv"
-              val vdif = config.input_directory + "/visit_dimension/patient_num=" + p + ".csv"
-              val ofif = config.input_directory + "/observation_fact/patient_num=" + p + ".csv"
-              val geodata_input_filename = config.input_directory + "/geodata/patient_num=" + p + ".csv"
-
-              val vdif_path = new Path(vdif)
-              val vdif_fs = vdif_path.getFileSystem(hc)
-              val ofif_path = new Path(ofif)
-              val ofif_fs = ofif_path.getFileSystem(hc)
-              val geodata_input_path = new Path(geodata_input_filename)
-              val geodata_input_fs = geodata_input_path.getFileSystem(hc)
-
-              if(!vdif_fs.exists(vdif_path) || !ofif_fs.exists(ofif_path) || !geodata_input_fs.exists(geodata_input_path)) {
-                println("of, vd, or geodata file not found, skipped " + p)
+              val output_filename = config.output_prefix + p
+              val output_file_path = new Path(output_filename)
+              val output_file_fs = output_file_path.getFileSystem(hc)
+              if(output_file_fs.exists(output_file_path)) {
+                println(output_file_path + " exists")
               } else {
-                println("loading patient_dimension from " + pdif)
-                val pddf = spark.read.format("csv").option("header", true).load(pdif)
-                println("loading observation_fact from " + ofif)
-                val ofdf = spark.read.format("csv").option("header", true).load(ofif)
-                println("loading visit_dimension from " + vdif)
-                val vddf = spark.read.format("csv").option("header", true).load(vdif)
-                println("loading geodata from " + geodata_input_filename)
-                val geodata_df = spark.read.format("csv").option("header", true).load(geodata_input_filename)
 
-                val pat = pddf.select("race_cd", "sex_cd", "birth_date")
+                val pdif = config.input_directory + "/patient_dimension/patient_num=" + p + ".csv"
+                val vdif = config.input_directory + "/visit_dimension/patient_num=" + p + ".csv"
+                val ofif = config.input_directory + "/observation_fact/patient_num=" + p + ".csv"
+                val geodata_input_filename = config.input_directory + "/geodata/patient_num=" + p + ".csv"
 
-                val lat = geodata_df.filter($"concept_cd".like("GEOLAT")).select("nval_num").agg(avg("nval_num").as("lat"))
+                val vdif_path = new Path(vdif)
+                val vdif_fs = vdif_path.getFileSystem(hc)
+                val ofif_path = new Path(ofif)
+                val ofif_fs = ofif_path.getFileSystem(hc)
+                val geodata_input_path = new Path(geodata_input_filename)
+                val geodata_input_fs = geodata_input_path.getFileSystem(hc)
 
-                val lon = geodata_df.filter($"concept_cd".like("GEOLONG")).select("nval_num").agg(avg("nval_num").as("lon"))
+                if (!vdif_fs.exists(vdif_path) || !ofif_fs.exists(ofif_path) || !geodata_input_fs.exists(geodata_input_path)) {
+                  println("of, vd, or geodata file not found, skipped " + p)
+                } else {
+                  println("loading patient_dimension from " + pdif)
+                  val pddf = spark.read.format("csv").option("header", true).load(pdif)
+                  println("loading observation_fact from " + ofif)
+                  val ofdf = spark.read.format("csv").option("header", true).load(ofif)
+                  println("loading visit_dimension from " + vdif)
+                  val vddf = spark.read.format("csv").option("header", true).load(vdif)
+                  println("loading geodata from " + geodata_input_filename)
+                  val geodata_df = spark.read.format("csv").option("header", true).load(geodata_input_filename)
 
-                val features = pat
-                  .crossJoin(lat)
-                  .crossJoin(lon)
+                  val pat = pddf.select("race_cd", "sex_cd", "birth_date")
 
-                // observation
-                val observation_cols = Seq(
-                  "valueflag_cd",
-                  "valtype_cd",
-                  "nval_num",
-                  "tval_char",
-                  "units_cd",
-                  "start_date",
-                  "end_date"
-                )
-                val observation = ofdf.select("encounter_num", "concept_cd", "instance_num", "modifier_cd", "valueflag_cd", "valtype_cd", "nval_num", "tval_char", "units_cd", "start_date", "end_date").orderBy("start_date")
+                  val lat = geodata_df.filter($"concept_cd".like("GEOLAT")).select("nval_num").agg(avg("nval_num").as("lat"))
 
-                val observation_wide = aggregate(observation, Seq("encounter_num", "concept_cd", "instance_num", "modifier_cd"), observation_cols, "observation")
+                  val lon = geodata_df.filter($"concept_cd".like("GEOLONG")).select("nval_num").agg(avg("nval_num").as("lon"))
 
-                // visit
-                val visit_cols = Seq(
-                  "inout_cd",
-                  "start_date",
-                  "end_date"
-                )
-                val visit = vddf.select("encounter_num", "inout_cd", "start_date", "end_date").orderBy("start_date")
+                  val features = pat
+                    .crossJoin(lat)
+                    .crossJoin(lon)
 
-                val visit_wide = aggregate(visit, Seq("encounter_num"), visit_cols, "visit")
+                  // observation
+                  val observation_cols = Seq(
+                    "valueflag_cd",
+                    "valtype_cd",
+                    "nval_num",
+                    "tval_char",
+                    "units_cd",
+                    "start_date",
+                    "end_date"
+                  )
+                  val observation = ofdf.select("encounter_num", "concept_cd", "instance_num", "modifier_cd", "valueflag_cd", "valtype_cd", "nval_num", "tval_char", "units_cd", "start_date", "end_date").orderBy("start_date")
 
-                //      val merge_map = udf((a:Map[String,Any], b:Map[String,Any]) => mergeMap(a,b))
+                  val observation_wide = aggregate(observation, Seq("encounter_num", "concept_cd", "instance_num", "modifier_cd"), observation_cols, "observation")
 
-                val features_wide = features
+                  // visit
+                  val visit_cols = Seq(
+                    "inout_cd",
+                    "start_date",
+                    "end_date"
+                  )
+                  val visit = vddf.select("encounter_num", "inout_cd", "start_date", "end_date").orderBy("start_date")
+
+                  val visit_wide = aggregate(visit, Seq("encounter_num"), visit_cols, "visit")
+
+                  //      val merge_map = udf((a:Map[String,Any], b:Map[String,Any]) => mergeMap(a,b))
+
+                  val features_wide = features
                     .crossJoin(observation_wide)
                     .crossJoin(visit_wide)
-                //        .select($"patient_num", $"encounter_num", $"sex_cd", $"race_cd", $"birth_date", merge_map($"observation", $"visit"))
+                  //        .select($"patient_num", $"encounter_num", $"sex_cd", $"race_cd", $"birth_date", merge_map($"observation", $"visit"))
 
-                // https://stackoverflow.com/questions/41601874/how-to-convert-row-to-json-in-spark-2-scala
-                val json = features_wide.toJSON.first()
+                  // https://stackoverflow.com/questions/41601874/how-to-convert-row-to-json-in-spark-2-scala
+                  val json = features_wide.toJSON.first()
 
-                writeToFile(hc, config.output_prefix + p, json)
+                  writeToFile(hc, output_filename, json)
+                }
+
               }
 
             }
