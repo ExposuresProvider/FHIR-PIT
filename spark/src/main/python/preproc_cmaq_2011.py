5c04c50002aa768bbf41c00aa996d773f6c1abe1
Hao Xu <xuh@email.unc.edu>
2018-03-28 17:24:28 -0400

    specify dir

--- spark/src/main/python/preproc_cmaq_2011.py
+++ spark/src/main/python/preproc_cmaq_2011.py
@@ -6,7 +6,11 @@ import pandas as pd
 input_dir = sys.argv[1]
 output_dir = sys.argv[2]
 
-dirs = sorted(glob.glob(os.path.join(input_dir, "C*")))
+if len(sys.argv) >= 4:
+    dirs = [os.path.join(input_dir, sys.argv[3])]
+else:
+    dirs = sorted(glob.glob(os.path.join(input_dir, "C*")))
+
 for i, dirname in enumerate(dirs):
     print("processing", i, "/", len(dirs), dirname)
     output_rowcol_filename = os.path.join(output_dir, os.path.basename(dirname) + ".csv")
