f0047c79a82f1fe3b4c996c30c4d9124af74cbee
Hao Xu <xuh@email.unc.edu>
2018-04-02 11:36:18 -0400

    join dataframes

--- spark/src/main/python/join.py
+++ spark/src/main/python/join.py
@@ -6,18 +6,19 @@ dir = sys.argv[1]
 
 output_file = sys.argv[2]
 
-def load(file):
-    pd.read_csv(file,sep="!")
 
-# https://stackoverflow.com/questions/29929639/when-combining-pandas-dataframe-concat-or-append-can-i-set-the-default-value
-def combine(a, b):
-    common_columns = a.columns.union(b.columns)
+df1 = None
+for file in glob.glob(dir, False):
+    df2 = pd.read_csv(file,sep="!")
+    if df1 is None:
+        df1 = df2
+    else:
+        # https://stackoverflow.com/questions/29929639/when-combining-pandas-dataframe-concat-or-append-can-i-set-the-default-value
+        common_columns = df1.columns.union(df2.columns)
+        df1 = df1.reindex(columns=common_columns, fill_value='')
+        df2 = df2.reindex(columns=common_columns, fill_value='')
 
-    df1 = a.reindex(columns=common_columns, fill_value='')
-    df2 = b.reindex(columns=common_columns, fill_value='')
+        df1 = pd.concat([df1, df2], axis=0, ignore_index=True)
 
-    return pd.concat([df1, df2], axis=0, ignore_index=True)
 
-df = reduce(combine, map(load, glob.glob(dir, False)))
-
-df.to_csv(output_file)
+df1.to_csv(output_file, sep="!", index=False)
