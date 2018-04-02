b9641d077ce820fc0d1283e9b54ce8a604f07a4f
Hao Xu <xuh@email.unc.edu>
2018-04-02 12:41:07 -0400

    avoid repeated reindexing

--- spark/src/main/python/join.py
+++ spark/src/main/python/join.py
@@ -7,24 +7,23 @@ dir = sys.argv[1]
 output_file = sys.argv[2]
 
 
-df1 = None
+dfs = []
 count = 0
+common_columns = []
 for file in glob.glob(dir):
     count += 1
     print("loading " + str(count) + " " + file)
     df2 = pd.read_csv(file,sep="!")
-    if df1 is None:
-        df1 = df2
-    else:
-        # https://stackoverflow.com/questions/29929639/when-combining-pandas-dataframe-concat-or-append-can-i-set-the-default-value
-        common_columns = df1.columns.union(df2.columns)
-        df1 = df1.reindex(columns=common_columns, fill_value='')
-        df2 = df2.reindex(columns=common_columns, fill_value='')
+    dfs.append((file, df2))
+    common_columns = common_columns.union(df2.columns)
 
-        df1 = pd.concat([df1, df2], axis=0, ignore_index=True)
+df1 = pd.DataFrame(columns = common_columns)
+count = 0
+# https://stackoverflow.com/questions/29929639/when-combining-pandas-dataframe-concat-or-append-can-i-set-the-default-value
+for file, df2 in dfs:
+    count += 1
+    print("reindexing " + str(count) + " " + file)
+    df2 = df2.reindex(columns=common_columns, fill_value='')
+    df1 = pd.concat([df1, df2], axis=0, ignore_index=True)
 
-    if count % 1000 == 0:
-        df1.to_csv(output_file, sep="!", index=False)
-
-if count % 1000 != 0:
-    df1.to_csv(output_file, sep="!", index=False)
+df1.to_csv(output_file, sep="!", index=False)
