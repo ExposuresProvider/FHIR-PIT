1d8708dac12903559523e0db214997b6884b3df3
Hao Xu <xuh@email.unc.edu>
2018-04-02 21:18:00 -0400

    adjust memory

--- spark/src/main/python/utils.py
+++ spark/src/main/python/utils.py
@@ -9,13 +9,13 @@ def submit(host_name, cache_dir, cls, *args, **kwargs):
            "--master",
            "spark://{0}:7077".format(host_name),
            "--executor-memory",
-           "2g",
+           "140g",
            "--driver-memory",
-           "2g",
+           "64g",
            "--num-executors",
            "1",
            "--executor-cores",
-           "16",
+           "30",
            "--jars",
            cache_dir + "/.ivy2/cache/com.github.scopt/scopt_2.11/jars/scopt_2.11-3.7.0.jar," +
            cache_dir + "/.ivy2/cache/com.typesafe.play/play-json_2.11/jars/play-json_2.11-2.6.7.jar," +
