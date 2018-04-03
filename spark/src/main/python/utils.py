ccf4f8f337df044d5cb5043842f375cedae61c6d
Hao Xu <xuh@email.unc.edu>
2018-04-02 21:13:27 -0400

    debug

--- spark/src/main/python/utils.py
+++ spark/src/main/python/utils.py
@@ -24,6 +24,7 @@ def submit(host_name, cache_dir, cls, *args, **kwargs):
            "--class",
            cls,
            "target/scala-2.11/preproc_2.11-1.0.jar"] + list(args)
+    print(cmd)
     if "log" in kwargs and "log2" in kwargs:
         log = kwargs["log"]
         log2 = kwargs["log2"]
