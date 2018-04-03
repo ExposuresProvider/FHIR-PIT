c84c5c734541582f74c09f25853241f6a2eeb00a
Hao Xu <xuh@email.unc.edu>
2018-04-02 20:38:57 -0400

    avoid repeated reindexing

--- spark/src/main/python/concat_dfs.py
+++ spark/src/main/python/concat_dfs.py
@@ -1,6 +1,5 @@
-import pandas as pd
 import sys
-import glob
+from utils import merge
 
 dir = sys.argv[1]
 
@@ -8,23 +7,4 @@ output_file = sys.argv[2]
 
 default_value = sys.argv[3]
 
-dfs = []
-count = 0
-common_columns = pd.Index([])
-for file in glob.glob(dir):
-    count += 1
-    print("loading " + str(count) + " " + file)
-    df2 = pd.read_csv(file,sep="!")
-    dfs.append((file, df2))
-    common_columns = common_columns.union(df2.columns)
-
-df1 = pd.DataFrame(columns = common_columns)
-count = 0
-# https://stackoverflow.com/questions/29929639/when-combining-pandas-dataframe-concat-or-append-can-i-set-the-default-value
-for file, df2 in dfs:
-    count += 1
-    print("reindexing " + str(count) + " " + file)
-    df2 = df2.reindex(columns=common_columns, fill_value=default_value)
-    df1 = pd.concat([df1, df2], axis=0, ignore_index=True)
-
-df1.to_csv(output_file, sep="!", index=False)
+merge(dir, output_file, default_value)
--- spark/src/main/python/utils.py
+++ spark/src/main/python/utils.py
@@ -1,7 +1,7 @@
 import subprocess
+import glob
+import pandas as pd
 from timeit import default_timer as timer
-
-
 
 def submit(host_name, cache_dir, cls, *args, **kwargs):
     start = timer()
@@ -38,3 +38,25 @@ def submit(host_name, cache_dir, cls, *args, **kwargs):
         print("error:", err)
     end = timer()
     print(end - start)
+
+def merge(dir, output_file, default_value):
+    dfs = []
+    count = 0
+    common_columns = pd.Index([])
+    for file in glob.glob(dir):
+        count += 1
+        print("loading " + str(count) + " " + file)
+        df2 = pd.read_csv(file,sep="!")
+        dfs.append((file, df2))
+        common_columns = common_columns.union(df2.columns)
+
+    df1 = pd.DataFrame(columns = common_columns)
+    count = 0
+    # https://stackoverflow.com/questions/29929639/when-combining-pandas-dataframe-concat-or-append-can-i-set-the-default-value
+    for file, df2 in dfs:
+        count += 1
+        print("reindexing " + str(count) + " " + file)
+        df2 = df2.reindex(columns=common_columns, fill_value=default_value)
+        df1 = pd.concat([df1, df2], axis=0, ignore_index=True)
+
+    df1.to_csv(output_file, sep="!", index=False)
