4fa297f92abb0e85488a53febb10f81da5a9413e
Hao Xu <xuh@email.unc.edu>
2018-04-06 00:44:39 -0400

    start end

--- spark/src/main/python/preproc_cmaq_2011.py
+++ spark/src/main/python/preproc_cmaq_2011.py
@@ -5,8 +5,11 @@ import pandas as pd
 
 input_dir = sys.argv[1]
 output_dir = sys.argv[2]
-start_date = sys.argv[3]
-end_date = sys.argv[4]
+if len(sys.argv) >= 4:
+    start_date = sys.argv[3]
+    end_date = sys.argv[4]
+else:
+    start_date = None
 
 if len(sys.argv) >= 6:
     dirs = [os.path.join(input_dir, sys.argv[5])]
@@ -20,11 +23,11 @@ for i, dirname in enumerate(dirs):
     def load_csv(filename):
         df = pd.read_csv(filename)
         df = df[["Date","O3_ppb", "PM25_Total_ugm3"]]
+        if start_date is not None:
+            df = df[(start_date <= pd.to_datetime(df["Date"])) & (pd.to_datetime(df["Date"]) < end_date)]
 
-        df2 = df[(start_date <= pd.to_datetime(df["Date"])) & (pd.to_datetime(df["Date"]) < end_date)]
-
-        df2.columns = ["start_date", "o3", "pm25"]
-        return df2
+        df.columns = ["start_date", "o3", "pm25"]
+        return df
     df = pd.concat(list(map (load_csv, filenames)))
 
     df.to_csv(output_rowcol_filename, index=False)
