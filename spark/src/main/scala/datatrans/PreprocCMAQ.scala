2baf85fc0763a02915106ec7eaf099445090fefa
Hao Xu <xuh@email.unc.edu>
2018-04-06 11:16:45 -0400

    copy

--- spark/src/main/scala/datatrans/PreprocCMAQ.scala
+++ spark/src/main/scala/datatrans/PreprocCMAQ.scala
@@ -1,11 +1,23 @@
 package datatrans
 
-import org.apache.hadoop.fs.{FileUtil, Path}
+import org.apache.hadoop.fs.{FileUtil, LocatedFileStatus, Path, RemoteIterator}
 import org.apache.spark.sql.SparkSession
 import datatrans.Utils._
 
+import scala.collection.mutable.ListBuffer
+
 
 object PreprocCMAQ {
+
+  def to_seq(itr: RemoteIterator[LocatedFileStatus]) : Array[Path] = {
+    val listBuf = new ListBuffer[Path]
+    while(itr.hasNext) {
+      val fstatus = itr.next()
+      listBuf.append(fstatus.getPath)
+    }
+    listBuf.toArray
+  }
+
   def main(args: Array[String]) {
     time {
       val spark = SparkSession.builder().appName("datatrans preproc").getOrCreate()
@@ -38,7 +50,8 @@ object PreprocCMAQ {
           val col = coldir.getName.split("=")(1).toInt
           val output_filename = output_dir + "/" + f"C$col%03dR$row%03d.csv"
           val output_file_path = new Path(output_filename)
-          FileUtil.copyMerge(output_dir_fs, coldir, output_dir_fs, output_file_path, true, hc, null)
+          val srcs = to_seq(output_dir_fs.listFiles(coldir, false))
+          FileUtil.copy(output_dir_fs, srcs, output_dir_fs, output_file_path, true, true, hc)
           prependStringToFile(hc, header, output_filename)
         }
       }
