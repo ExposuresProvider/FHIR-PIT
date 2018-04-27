f8e69dff10c11bbfaab2de0084d14f195e4cae77
Hao Xu <xuh@email.unc.edu>
2018-04-27 11:52:11 -0400

    commandline options more fixes

--- spark/src/main/python/merge_dfs.py
+++ spark/src/main/python/merge_dfs.py
@@ -5,7 +5,7 @@ output_dir = sys.argv[1]
 input_dirs = sys.argv[2::4]
 pats = sys.argv[3::4]
 how = sys.argv[4::4]
-on = map(lambda x : x.split(","), sys.argv[5::4])
+on = list(map(lambda x : x.split(","), sys.argv[5::4]))
 
 
 merge(input_dirs, pats, how, on, output_dir, 30)
