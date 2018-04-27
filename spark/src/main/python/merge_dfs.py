ca55c3368841b3b0d9f55283c83a405abb181a7b
Hao Xu <xuh@email.unc.edu>
2018-04-27 12:28:51 -0400

    parsecols

--- spark/src/main/python/merge_dfs.py
+++ spark/src/main/python/merge_dfs.py
@@ -5,7 +5,12 @@ output_dir = sys.argv[1]
 input_dirs = sys.argv[2::4]
 pats = sys.argv[3::4]
 hows = sys.argv[4::4]
-ons = list(map(lambda x : x.split(","), sys.argv[5::4]))
+def parsecols(x):
+    if x == "":
+        return []
+    else:
+        return x.split(",")
+ons = list(map(parsecols, sys.argv[5::4]))
 
 
 merge(input_dirs, pats, hows, ons, output_dir, 30)
