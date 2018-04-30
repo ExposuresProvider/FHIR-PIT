5d3684ff0af3ee92ae1bdf546bfb2626c764487a
Lisa Stillwell <lisa@renci.org>
2018-04-30 16:18:19 -0400

    added new functionality to find GEOID, given lat lon

--- spark/src/main/java/datatrans/Main.java
+++ spark/src/main/java/datatrans/Main.java
@@ -4,12 +4,14 @@ public class Main {
 	
 	public static void main(String[] args) throws Exception {
 		double lon = -80.482415;
-		//double lat = 36.352975;
 		double lat = 36.361;
+		
 		NearestRoad nr = new NearestRoad("/Users/lisa/RDP_Share/GIS/tl_2015_allstates_prisecroads_lcc.shp");
 		double distance = nr.getMinimumDistance(lat, lon);
-		
 		System.out.println("Nearest primary or secondary road, " + nr.getMatchedRoadName() + ", is " + (int)distance + " meters away from point " + lon + ", " + lat);
+		
+		GeoidFinder gf = new GeoidFinder("/Users/lisa/RDP_Share/GIS/census_tracts/tl_2016_37_bg_lcc.shp");
+		String geoid = gf.getGeoidForLatLon(lat, lon);
+		System.out.println("GEOID = " + geoid);
 	}
-
 }
