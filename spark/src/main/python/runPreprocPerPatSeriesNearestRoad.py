865f1fa06bfdd7013b9536f32bc9cd02b4ed286e
Hao Xu <xuh@cs.unc.edu>
2018-05-01 16:57:00 -0400

    nearing road python

---
+++ spark/src/main/python/runPreprocPerPatSeriesNearestRoad.py
@@ -0,0 +1,20 @@
+import os
+import sys
+from utils import submit
+
+input_dir = sys.argv[1]
+time_series = sys.argv[2]
+patient_dimension = sys.argv[3]
+environmental_data = sys.argv[4]
+output_dir = sys.argv[5]
+cache_dir = sys.argv[6]
+host_name = sys.argv[7]
+
+
+submit(host_name, cache_dir, "datatrans.PreprocPerPatSeriesEnvData",
+           "--patient_dimension={0}".format(patient_dimension),
+           "--input_directory=" + input_dir + "/",
+           "--time_series=" + time_series,
+           "--output_file=" + output_dir + "/",
+           "--nearestroad_data=" + environmental_data, *sys.argv[8:])
+
