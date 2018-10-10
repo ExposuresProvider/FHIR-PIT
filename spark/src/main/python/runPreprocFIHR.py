6a8c49de3cad953c3f8a70c585c0e84fb7bc990c
Hao Xu <xuh@cs.unc.edu>
2018-10-10 15:24:36 -0400

    update PreprocFIHR

--- spark/src/main/python/runPreprocFIHR.py
+++ spark/src/main/python/runPreprocFIHR.py
@@ -3,13 +3,20 @@ from utils import submit
 
 cache_dir = sys.argv[1]
 host_name = sys.argv[2]
-patient_dimension = sys.argv[3]
+input_dir = sys.argv[3]
 resc_types = sys.argv[4]
-output_dir = sys.argv[5]
+skip_preproc = sys.argv[5]
+replace_pat = bool(sys.argv[6])
+output_dir = sys.argv[7]
 
+args = []
+if replace_pat:
+    args.append("--replace_pat")
+args.extend(sys.argv[8:])
 
 submit(host_name, cache_dir, "datatrans.PreprocFIHR",
            "--input_dir={0}".format(patient_dimension),
            "--resc_types=" + resc_types,
-           "--output_dir=" + output_dir, *sys.argv[6:])
+           "--skip_preproc" + skip_preproc,
+           "--output_dir=" + output_dir, *args)
 
