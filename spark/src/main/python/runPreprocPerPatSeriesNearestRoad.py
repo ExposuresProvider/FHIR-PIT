00aec1eb09167456258c012255ad9348af3c250e
Hao Xu <xuh@cs.unc.edu>
2018-10-15 02:12:55 -0400

    update PreprocPerPatSeriesNearestRoad

--- spark/src/main/python/runPreprocPerPatSeriesNearestRoad.py
+++ spark/src/main/python/runPreprocPerPatSeriesNearestRoad.py
@@ -4,15 +4,13 @@ from utils import submit
 
 cache_dir = sys.argv[1]
 host_name = sys.argv[2]
-patient_dimension = sys.argv[3]
 time_series = sys.argv[4]
 environmental_data = sys.argv[5]
 output_dir = sys.argv[6]
 
 
 submit(host_name, cache_dir, "datatrans.PreprocPerPatSeriesNearestRoad",
-           "--patient_dimension={0}".format(patient_dimension),
-           "--time_series=" + time_series,
+           "--patgeo_data=" + time_series,
            "--output_file=" + output_dir,
            "--nearestroad_data=" + environmental_data, *sys.argv[7:])
 
--- spark/src/main/scala/datatrans/PreprocPerPatSeriesNearestRoad.scala
+++ spark/src/main/scala/datatrans/PreprocPerPatSeriesNearestRoad.scala
@@ -9,8 +9,7 @@ import play.api.libs.json._
 import scopt._
 
 case class PreprocPerPatSeriesNearestRoadConfig(
-                   patient_dimension : String = "",
-                   time_series : String = "",
+                   patgeo_data : String = "",
                    nearestroad_data : String = "",
                    maximum_search_radius : Double = 500,
                    output_file : String = ""
@@ -18,51 +17,10 @@ case class PreprocPerPatSeriesNearestRoadConfig(
 
 object PreprocPerPatSeriesNearestRoad {
 
-  def proc_pid(config : PreprocPerPatSeriesNearestRoadConfig, spark: SparkSession, nearestRoad: NearestRoad, p:String): Option[(String, Double)] =
-    time {
-
-      val hc = spark.sparkContext.hadoopConfiguration
-
-      val input_file = f"${config.time_series}/$p"
-      val input_file_path = new Path(input_file)
-      val input_file_file_system = input_file_path.getFileSystem(hc)
-
-      if(!input_file_file_system.exists(input_file_path)) {
-        println("json not found, skipped " + p)
-        None
-      } else {
-        println("loading json from " + input_file)
-        val input_file_input_stream = input_file_file_system.open(input_file_path)
-
-
-        val jsvalue = Json.parse(input_file_input_stream)
-        input_file_input_stream.close()
-
-        jsvalue \ "lat" match {
-          case JsUndefined() =>
-            println("lat doesn't exists")
-            None
-          case JsDefined(latitutestr) =>
-            val lat = latitutestr.as[Double]
-            jsvalue \ "lon" match {
-              case JsUndefined() =>
-                println("lon doesn't exists")
-                None
-              case JsDefined(lons) =>
-                val lon = lons.as[Double]
-
-                Some((p, nearestRoad.getMinimumDistance(lat, lon)))
-            }
-        }
-
-      }
-    }
-
   def main(args: Array[String]) {
     val parser = new OptionParser[PreprocPerPatSeriesNearestRoadConfig]("series_to_vector") {
       head("series_to_vector")
-      opt[String]("patient_dimension").required.action((x,c) => c.copy(patient_dimension = x))
-      opt[String]("time_series").required.action((x,c) => c.copy(time_series = x))
+      opt[String]("patgeo_data").required.action((x,c) => c.copy(patgeo_data = x))
       opt[String]("nearestroad_data").required.action((x,c) => c.copy(nearestroad_data = x))
       opt[Double]("maximum_search_radius").action((x,c) => c.copy(maximum_search_radius = x))
       opt[String]("output_file").required.action((x,c) => c.copy(output_file = x))
@@ -80,12 +38,6 @@ object PreprocPerPatSeriesNearestRoad {
 
         time {
 
-          println("loading patient_dimension from " + config.patient_dimension)
-          val pddf0 = spark.read.format("csv").option("header", value = true).load(config.patient_dimension)
-          val patl = pddf0.select("patient_num").map(r => r.getString(0)).collect.toList
-
-          val count = new AtomicInteger(0)
-          val n = patl.size
           val hc = spark.sparkContext.hadoopConfiguration
           val output_file_path = new Path(config.output_file)
           val output_file_file_system = output_file_path.getFileSystem(hc)
@@ -93,14 +45,15 @@ object PreprocPerPatSeriesNearestRoad {
           if(output_file_file_system.exists(output_file_path)) {
             println(config.output_file + " exists")
           } else {
-
-            val nearestRoad = new NearestRoad(f"${config.nearestroad_data}", config.maximum_search_radius)
-            val rows = patl.par.flatMap(pid => {
-              println("processing " + count.incrementAndGet + " / " + n + " " + pid)
-              proc_pid(config, spark, nearestRoad, pid)
+            val pddf0 = spark.read.format("csv").option("header", value = true).load(config.patgeo_data)
+            val rows = pddf0.mapPartitions(partition => {
+              val nearestRoad = new NearestRoad(config.nearestroad_data, config.maximum_search_radius)
+              partition.map(r => {
+                (r.getString(0), nearestRoad.getMinimumDistance(r.getString(1).toDouble, r.getString(2).toDouble))
+              })
             })
 
-            val df = rows.toList.toDF("patient_num", "distant_to_nearest_road")
+            val df = rows.toDF("patient_num", "distant_to_nearest_road")
 
             writeDataframe(hc, config.output_file, df)
           }
@@ -110,9 +63,7 @@ object PreprocPerPatSeriesNearestRoad {
       case None =>
     }
 
-
-  spark.stop()
-
+    spark.stop()
 
   }
 
