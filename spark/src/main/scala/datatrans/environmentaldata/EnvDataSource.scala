497103d9bc04fdaee662ad2def831757724f5867
Hao Xu <xuh@cs.unc.edu>
2018-10-15 06:30:33 -0400

    update EnvDataSource

--- spark/src/main/scala/datatrans/environmentaldata/EnvDataSource.scala
+++ spark/src/main/scala/datatrans/environmentaldata/EnvDataSource.scala
@@ -73,7 +73,7 @@ class EnvDataSource(config: EnvDataSourceConfig) {
     if (dfs.nonEmpty && dfs2.nonEmpty) {
       val df = dfs.reduce((a, b) => a.union(b))
       val df2 = dfs2.reduce((a, b) => a.union(b))
-      val df3 = df2.withColumn("start_date", to_date(df2.col("Date"),"yy/MM/dd"))
+      val df3 = df2.withColumn("start_date", to_date(df2.col("Date"),"yy/MM/dd")).drop("Date")
       Some(df.join(df3, Seq("start_date"), "outer"))
 
     } else {
