4d0cd202050f106ecfb1fbb50afe66861e5f3f44
Hao Xu <xuh@cs.unc.edu>
2018-10-17 22:36:53 -0400

    update PreprocPerPatSeriesToVector

--- spark/src/main/scala/datatrans/Utils.scala
+++ spark/src/main/scala/datatrans/Utils.scala
@@ -1,5 +1,6 @@
 package datatrans
 
+import java.util.concurrent.atomic.AtomicInteger
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs._
 import org.apache.spark.sql.{DataFrame, Row, SparkSession}
