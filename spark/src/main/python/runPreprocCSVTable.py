d86f68ef1b34c096964efcb2b98ec298fea032f7
Hao Xu <xuh@cs.unc.edu>
2018-10-19 19:49:42 -0400

    update PreprocAggregate

--- spark/src/main/python/runPreprocCSVTable.py
+++ spark/src/main/python/runPreprocCSVTable.py
@@ -5,14 +5,14 @@ from utils import submit
 cache_dir = sys.argv[1]
 host_name = sys.argv[2]
 patient_dir = sys.argv[3]
-environment_dir = sys.argv[3]
-input_files = sys.argv[3]
-output_dir = sys.argv[4]
+environment_dir = sys.argv[4]
+input_files = sys.argv[5]
+output_dir = sys.argv[6]
 
 
 submit(host_name, cache_dir, "datatrans.PreprocCSVTable",
            "--patient_directory=" + patient_dir,
            "--environment_directory=" + environment_dir,
            "--input_files=" + input_files,
-           "--output_directory=" + output_dir, *sys.argv[5:])
+           "--output_directory=" + output_dir, *sys.argv[7:])
 
