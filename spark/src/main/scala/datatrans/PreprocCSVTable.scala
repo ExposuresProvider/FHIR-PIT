338cf329445e8283de73283a93a0e1894b81c4db
Hao Xu <xuh@cs.unc.edu>
2018-11-29 11:50:53 -0500

    update

--- spark/src/main/scala/datatrans/PreprocCSVTable.scala
+++ spark/src/main/scala/datatrans/PreprocCSVTable.scala
@@ -79,11 +79,16 @@ object PreprocCSVTable {
                 val pat_df = spark.read.format("csv").option("header", value = true).load(f.toString())
 
                 if(!pat_df.head(1).isEmpty) {
-                  val env_df = spark.read.format("csv").option("header", value = true).load(config.environment_file + "/" + p)
-                  val env_df2 = env_df.withColumn("next_date", plusOneDayDate(env_df.col("start_date"))).drop("start_date").withColumnRenamed("next_date", "start_date")
+                  val env_file = config.environment_file + "/" + p
+                  if(fileExists(hc, env_file)) {
+                    val env_df = spark.read.format("csv").option("header", value = true).load()
+                    val env_df2 = env_df.withColumn("next_date", plusOneDayDate(env_df.col("start_date"))).drop("start_date").withColumnRenamed("next_date", "start_date")
                   
-                  val patenv_df = pat_df.join(env_df2, "start_date").join(df, "patient_num")
-                  writeDataframe(hc, output_file, patenv_df)
+                    val patenv_df = pat_df.join(env_df2, "start_date").join(df, "patient_num")
+                    writeDataframe(hc, output_file, patenv_df)
+                  } else {
+                    println("warning: no record is contructed because env file does not exist for " + p)
+                  }
                 }
               }
 
