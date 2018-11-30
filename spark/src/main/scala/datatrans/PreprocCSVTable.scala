75b77d2c234af2c39f724ef0341861dfa819e692
Hao Xu <xuh@cs.unc.edu>
2018-11-30 12:11:09 -0500

    update

--- spark/src/main/scala/datatrans/PreprocCSVTable.scala
+++ spark/src/main/scala/datatrans/PreprocCSVTable.scala
@@ -243,6 +243,7 @@ object PreprocCSVTable {
             df_all_visit.columns.seq.foreach(println)
             df_all_visit = df_all_visit
               .withColumnRenamed("ObesityBMIVisit", "ObesityBMIVisit0")
+            df_all_visit = df_all_visit
               .withColumn("ObesityBMIVisit", procObesityBMI(df.col("ObesityBMIVisit0")))
               .drop("ObesityBMIVisit0")
 
@@ -264,6 +265,7 @@ object PreprocCSVTable {
             var df_all_patient = df_all2.drop(deidentify2 : _*)
             df_all_patient = df_all_patient
               .withColumnRenamed("ObesityBMI", "ObesityBMI0")
+            df_all_patient = df_all_patient
               .withColumn("ObesityBMI", procObesityBMI(df.col("ObesityBMI0")))
               .drop("ObesityBMI0")
             writeDataframe(hc, output_all_patient, df_all_patient)
