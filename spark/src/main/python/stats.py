a7d0d2d3bc399ca5cb7f458c819eb29fd62772af
Hao Xu <xuh@cs.unc.edu>
2019-01-26 03:35:00 -0500

    update

--- spark/src/main/python/stats.py
+++ spark/src/main/python/stats.py
@@ -9,4 +9,6 @@ df = pd.read_csv(filename)
 columns = list(df.columns.values)
 
 for column in columns:
-    print(df[column].value_counts())
+    vc = df[column].value_counts().sort_index()
+    with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+      print(df)
