99961e05d91ac4bb12e8bb2991616d62cb0ef21c
Hao Xu <xuh@cs.unc.edu>
2019-02-06 01:26:28 -0500

    update

--- spark/src/main/scala/datatrans/PreprocCSVTable.scala
+++ spark/src/main/scala/datatrans/PreprocCSVTable.scala
@@ -48,7 +48,7 @@ object PreprocCSVTable {
     import spark.implicits._
 
     val env_schema = StructType(
-      StructField("start_date", DateType, true) :: List("o3_avg", "pm25_avg", "o3_max", "pm25_max", "ozone_daily_8hour_maximum", "pm25_daily_average").flatMap(i => List(i, i + "_avg", i + "_max")).map(x => StructField(x, DoubleType, false)))
+      StructField("start_date", DateType, true) :: (for(f <- Seq((i:String)=>i, (i:String)=>i + "_avg", (i:String)=>i + "_max"); name <- Seq("o3_avg", "pm25_avg", "o3_max", "pm25_max", "ozone_daily_8hour_maximum", "pm25_daily_average")) yield f(name)).toList.map(x => StructField(x, DoubleType, false)))
 
     parser.parse(args, PreprocCSVTableConfig()) match {
       case Some(config) =>
