53282fe72d59434374b1184fbc9a0bc5132f6029
Hao Xu <xuh@cs.unc.edu>
2019-02-07 17:26:56 -0500

    update

--- spark/src/main/python/stats.py
+++ spark/src/main/python/stats.py
@@ -1,18 +1,35 @@
 import pandas as pd
 
 import sys
+from tabulate import tabulate
 
 filename = sys.argv[1]
+filename2 = sys.argv[2]
 
 df = pd.read_csv(filename)
+df2 = pd.read_csv(filename2)
 
 columns = list(df.columns.values)
+columns2 = list(df2.columns.values)
 
-for column in columns:
+columnSet = set(columns)
+columnSet2 = set(columns2)
+
+print("columns in first file not in second file:", columnSet - columnSet2)
+print("columns not in first file in second file:", columnSet2 - columnSet)
+
+for column in [column for column in columns if column in columns2]:
+    print("column:", column)
     vc = df[column].value_counts().sort_index()
-    if vc.count() <= 10:
-        with pd.option_context('display.max_rows', None, 'display.max_columns', None):
-            print(vc)
-    else:
-        print(df[column].describe())
+    vc2 = df2[column].value_counts().sort_index()
+
+    if vc.count() > 10:
+        vc = vc.describe()
+        vc2 = vc2.describe()
+
+    indices = list(vc.index.values)
+    indices2 = list(vc2.index.values)
+
+    print(tabulate([[index, vc.get(index, default=0), vc2.get(index, default=0)] for index in indices + [index for index in indices2 if index not in indices]]))
+            
 
