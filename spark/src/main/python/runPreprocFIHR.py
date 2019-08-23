c73ec7538e2358f0b8d92853ac55f5a712f1a3eb
Hao Xu <xuh@cs.unc.edu>
2019-08-23 11:50:15 -0400

    yaml

--- spark/src/main/python/runPreprocFIHR.py
+++ spark/src/main/python/runPreprocFIHR.py
@@ -1,14 +1,6 @@
 import sys
 from utils import submit
 
-host_name, input_dir, resc_types, skip_preproc, output_dir, *args = sys.argv[1:]
-
- = []
-args.extend(sys.argv[6:])
-
-submit(host_name, cache_dir, "datatrans.PreprocFIHR",
-           "--input_dir=" + input_dir,
-           "--resc_types=" + resc_types,
-           "--skip_preproc=" + skip_preproc,
-           "--output_dir=" + output_dir, *args)
+host_name, config = sys.argv[1:]
+submit(host_name, "datatrans.PreprocFIHR", "--config=" + config)
 
