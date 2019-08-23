f10c9cad2e9a645412dfad2d63f984ba8bcfb123
Hao Xu <xuh@cs.unc.edu>
2019-08-23 11:42:08 -0400

    yaml

---
+++ spark/src/main/scala/datatrans/Config.scala
@@ -0,0 +1,30 @@
+package datatrans
+
+import scopt._
+import net.jcazevedo.moultingyaml._
+
+case class InputConfig(
+  config : String = ""
+)
+
+object Config {
+
+  def parseInput[T](args : Seq[String], configFormatExplicit : YamlFormat[T]) : Option[T] = {
+
+    val parser = new OptionParser[InputConfig]("series_to_vector") {
+      head("series_to_vector")
+      opt[String]("config").required.action((x,c) => c.copy(config = x))
+    }
+
+    parser.parse(args, InputConfig()).map(
+      configFile => {
+        implicit val configFormat = configFormatExplicit
+        val source = scala.io.Source.fromFile(configFile.config)
+        val yamlStr = try source.mkString finally source.close()
+        yamlStr.parseYaml.convertTo[T]
+      }
+    )
+
+  }
+
+}
