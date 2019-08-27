614ea3277be34224dc7ba6c4a5e51dccbd1a6e3b
Hao Xu <xuh@cs.unc.edu>
2019-08-27 09:47:01 -0400

    update run script

--- spark/src/main/python/runPreprocPerPatSeries.py
+++ spark/src/main/python/runPreprocPerPatSeries.py
@@ -1,19 +1,7 @@
-import os
-import sys
-import subprocess
-from timeit import default_timer as timer
 import sys
 from utils import submit
 
-dir = sys.argv[1]
-patient_dimension = sys.argv[2]
-patient_series = sys.argv[3]
-output_dir = sys.argv[4]
-cache_dir = sys.argv[5]
-host_name = sys.argv[6]
+host_name, config = sys.argv[1:]
+submit(host_name, "datatrans.PreprocPerPatSeries", "--config=" + config)
 
-submit(host_name, cache_dir, "datatrans.PreprocPerPatSeries",
-       "--patient_dimension={0}/{1}".format(dir, patient_dimension),
-       "--input_directory=" + dir + "/" + patient_series,
-       "--output_prefix=" + output_dir + "/")
 
