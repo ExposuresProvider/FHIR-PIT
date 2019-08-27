44315adaf7971e6d86c245207f4209795b58b4b8
Hao Xu <xuh@cs.unc.edu>
2019-08-27 09:48:59 -0400

    update runPreprocPerPatSeriesToVector

--- spark/src/main/python/runPreprocPerPatSeriesToVector.py
+++ spark/src/main/python/runPreprocPerPatSeriesToVector.py
@@ -1,17 +1,5 @@
-import os
 import sys
 from utils import submit
 
-cache_dir = sys.argv[1]
-host_name = sys.argv[2]
-input_dir = sys.argv[3]
-med_map = sys.argv[4]
-output_dir = sys.argv[5]
-
-
-submit(host_name, cache_dir, "datatrans.PreprocPerPatSeriesToVector",
-       "--input_directory=" + input_dir,
-       "--output_directory=" + output_dir,
-       "--mdctn_rxnorm=" + med_map,
-       *sys.argv[6:])
-
+host_name, config = sys.argv[1:]
+submit(host_name, "datatrans.PreprocPerPatSeriesToVector", "--config=" + config)
