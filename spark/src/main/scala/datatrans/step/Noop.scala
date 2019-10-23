ce2195611300ec2d13acb61b8152b9b438fec859
Hao Xu <xuh@cs.unc.edu>
2019-10-23 15:08:00 -0400

    update

---
+++ spark/src/main/scala/datatrans/step/Noop.scala
@@ -0,0 +1,21 @@
+package datatrans.step
+
+import datatrans.Utils._
+import org.apache.spark.sql.SparkSession
+import datatrans.Config._
+import net.jcazevedo.moultingyaml._
+import net.jcazevedo.moultingyaml.DefaultYamlProtocol._
+import datatrans.Implicits._
+import datatrans._
+
+object Noop extends StepConfigConfig {
+
+  type ConfigType = Unit
+
+  val yamlFormat = UnitYamlFormat
+
+  val configType = "Noop"
+
+  def step(spark: SparkSession, config: Unit): Unit = ()
+
+}
