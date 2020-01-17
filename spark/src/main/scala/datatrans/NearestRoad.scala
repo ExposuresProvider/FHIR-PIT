32b718c36a7fa5b594591191491e18b20182c847
Hao Xu <xuh@cs.unc.edu>
2020-01-17 18:48:11 -0500

    convert Long to String

--- spark/src/main/scala/datatrans/NearestRoad.scala
+++ spark/src/main/scala/datatrans/NearestRoad.scala
@@ -107,7 +107,7 @@ class NearestRoad(roadShapefilePath : String, maximum_search_radius : Double) {
 
   def getMatchedRouteId : Option[String] = lastMatched.map(_.getAttribute("ROUTE_ID").asInstanceOf[String])
   
-  def getMatchedNumLanes : Option[String] = lastMatched.map(_.getAttribute("THROUGH_LA").asInstanceOf[String])
+  def getMatchedNumLanes : Option[String] = lastMatched.map(_.getAttribute("THROUGH_LA").asInstanceOf[Long].toString)
   
   def getMatchedSpeed : Option[String] = lastMatched.map(_.getAttribute("SPEED").asInstanceOf[Long].toString)
   
