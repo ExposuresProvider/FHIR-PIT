906c9661dba895026c846c0b1f8ed08f8a1d3d13
Hao Xu <xuh@cs.unc.edu>
2020-03-04 04:00:48 -0500

    remove caching

--- spark/src/main/scala/datatrans/environmentaldata/EnvDataSource.scala
+++ spark/src/main/scala/datatrans/environmentaldata/EnvDataSource.scala
@@ -15,6 +15,7 @@ import datatrans.environmentaldata.Utils._
 
 class EnvDataSource(spark: SparkSession, config: EnvDataCoordinatesConfig) {
   val log = Logger.getLogger(getClass.getName)
+  val hc = spark.sparkContext.hadoopConfiguration
 
   log.setLevel(Level.INFO)
   val envSchema = StructType(
@@ -22,15 +23,20 @@ class EnvDataSource(spark: SparkSession, config: EnvDataCoordinatesConfig) {
       (for(j <- Seq("avg", "max", "min", "stddev"); i <- Seq("o3", "pm25")) yield i + "_" + j).map(i => StructField(i, DoubleType)).toList
   )
 
-  def loadEnvDataFrame(input: (String, Seq[String], StructType)) : Option[DataFrame] = {
-    val (filename, names, schema) = input
-    val df = readCSV(spark, filename, schema, (_: String) => DoubleType)
-    if (names.forall(x => df.columns.contains(x))) {
-      Some(df.cache())
+  def loadEnvDataFrame(filename: String, names: Seq[String], schema: StructType) : Option[DataFrame] = {
+    val filepath = new Path(filename)
+    val file_file_system = filepath.getFileSystem(hc)
+    if (file_file_system.exists(filepath)) {
+      val df = readCSV(spark, filename, schema, (_: String) => DoubleType)
+      if (names.forall(x => df.columns.contains(x))) {
+        Some(df)
+      } else {
+        log.error(f"$filename doesn't contain all required columns")
+        val namesNotInDf = names.filter(x => !df.columns.contains(x))
+        Some(namesNotInDf.foldLeft(df)((df : DataFrame, x : String) => df.withColumn(x, lit(null))))
+      }
     } else {
-      log.error(f"$filename doesn't contain all required columns")
-      val namesNotInDf = names.filter(x => !df.columns.contains(x))
-      Some(namesNotInDf.foldLeft(df)((df : DataFrame, x : String) => df.withColumn(x, lit(null))))
+      None
     }
   }
     
@@ -39,10 +45,10 @@ class EnvDataSource(spark: SparkSession, config: EnvDataCoordinatesConfig) {
     val dfs = coors.flatMap {
       case (year, (row, col)) =>
         val filename = f"${config.environmental_data}/cmaq$year/C$col%03dR$row%03dDaily.csv"
-        loadEnvDataFrameCache((filename, config.indices, envSchema))
+        loadEnvDataFrame(filename, config.indices, envSchema)
     }
     if (dfs.nonEmpty) {
-      dfs.reduce((a, b) => a.union(b)).cache()
+      dfs.reduce((a, b) => a.unionByName(b))
     } else {
       log.error(f"input env df is not available ${coors}")
       spark.createDataFrame(spark.sparkContext.emptyRDD[Row], envSchema)
@@ -52,14 +58,10 @@ class EnvDataSource(spark: SparkSession, config: EnvDataCoordinatesConfig) {
   def stats(names2 : Seq[String]) = names2 ++ (for (i <- config.statistics; j <- names2) yield f"${j}_$i") ++ (for (j <- names2) yield f"${j}_prev_date")
 
   def generateOutputDataFrame(coors: Seq[(Int, (Int, Int))]) = {
-    val df = loadRowColDataFrameCache(coors)
+    val df = loadRowColDataFrame(coors)
     val dfyear = aggregateByYear(spark, df, config.indices, config.statistics, Seq())
-    dfyear.select("start_date", ("year" +: stats(config.indices)): _*).cache()
+    dfyear.select("start_date", ("year" +: stats(config.indices)): _*)
   }
-
-  private val loadEnvDataFrameCache = new Cache(loadEnvDataFrame)
-  private val loadRowColDataFrameCache = new Cache(loadRowColDataFrame)
-  private val generateOutputDataFrameCache = new Cache(generateOutputDataFrame)
 
   def run(): Unit = {
 
@@ -72,32 +74,24 @@ class EnvDataSource(spark: SparkSession, config: EnvDataCoordinatesConfig) {
 
       val patl = pddf0.select("patient_num", "lat", "lon").map(r => (r.getString(0), r.getString(1).toDouble, r.getString(2).toDouble)).collect.toList
 
-      val hc = spark.sparkContext.hadoopConfiguration
 
       val n = patl.size
 
       withCounter(count => 
 
-      patl.par.foreach{
-        case (r, lat, lon) =>
-          log.info("processing patient " + count.incrementAndGet() + " / " + n + " " + r)
-          val timeZone = DateTimeZone.forOffsetHours(config.offset_hours)
-          val start_date_local = config.start_date.toDateTime(timeZone)
-          val end_date_local = config.end_date.toDateTime(timeZone).minusDays(1)
-          val yearseq = start_date_local.year.get to end_date_local.year.get
-          val output_file = f"${config.output_dir}/$r"
-          log.info(f"loading env data from year sequence $yearseq, start_date = ${start_date_local}, end_date = ${end_date_local}")
-          val coors = yearseq.intersect(Seq(2010,2011)).flatMap(year => {
-            latlon2rowcol(lat, lon, year) match {
-              case Some((row, col)) =>
-                Seq((year, (row, col)))
-              case _ =>
-                Seq()
-            }
-          })
+        patl.par.foreach{
+          case (r, lat, lon) =>
+            log.info("processing patient " + count.incrementAndGet() + " / " + n + " " + r)
+            val timeZone = DateTimeZone.forOffsetHours(config.offset_hours)
+            val start_date_local = config.start_date.toDateTime(timeZone)
+            val end_date_local = config.end_date.toDateTime(timeZone).minusDays(1)
+            val yearseq = start_date_local.year.get to end_date_local.year.get
+            val output_file = f"${config.output_dir}/$r"
+            log.info(f"loading env data from year sequence $yearseq, start_date = ${start_date_local}, end_date = ${end_date_local}")
+            val coors = yearseq.flatMap(year => latlon2rowcol(lat, lon, year).map(rc => (year, rc)))
 
-          val df = generateOutputDataFrameCache(coors)
-          writeDataframe(hc, output_file, df)
+            val df = generateOutputDataFrame(coors)
+            writeDataframe(hc, output_file, df)
 
       })
     }
