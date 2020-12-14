2edeeba7ac5707d24a064ee3b0b27fee3806d790
Hao Xu <xuh@cs.unc.edu>
2020-12-13 23:11:40 -0500

    update

---
+++ spark/src/main/scala/datatrans/step/PreprocPerPatSeriesCSVTableLocal.scala
@@ -0,0 +1,58 @@
+package datatrans.step
+
+import sys.process.{Process, ProcessLogger}
+import sys.process._
+import java.io.PrintWriter
+import java.nio.file.Files
+import scala.reflect.io.Directory
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.Path
+import org.apache.spark.sql.types.{StructType, StructField, DateType, DoubleType}
+import org.apache.spark.sql.{Column, Row, SparkSession, DataFrame}
+import org.joda.time.format.{ISODateTimeFormat, DateTimeFormat}
+import org.joda.time.{DateTime, DateTimeZone}
+import org.apache.spark.sql.functions.udf
+import io.circe.{Decoder, Encoder}
+import io.circe.syntax._
+import io.circe.generic.semiauto.{deriveDecoder, deriveEncoder}
+import datatrans.Utils.{fileExists, readCSV2, time, withCounter, HDFSCollection, writeDataframe}
+import datatrans.StepImpl
+import datatrans.Mapper
+
+object PreprocPerPatSeriesCSVTableLocal extends StepImpl {
+  
+  type ConfigType = PreprocPerPatSeriesCSVTableConfig
+
+  import datatrans.SharedImplicits._
+
+  val configDecoder : Decoder[ConfigType] = deriveDecoder
+  implicit val configEncoder : Encoder[ConfigType] = deriveEncoder
+
+  def step(spark: SparkSession, config:PreprocPerPatSeriesCSVTableConfig) : Unit = {
+
+    time {
+      val tempFi = Files.createTempFile(null, null)
+      val tempDir = Files.createTempDirectory(null)
+      try {
+        val w = new PrintWriter(tempFi.toFile)
+
+        try {
+          w.write(config.asJson.toString)
+        } finally {
+          w.close()
+        }
+
+        f"virtualenv -p /usr/bin/python3 ${tempDir.toString}/venv".!
+        f"${tempDir.toString}/venv/bin/pip install -r requirements.txt".!
+        val exitValue = Process(f"${tempDir.toString}/venv/bin/python src/main/python/perPatSeriesCSVTable.py --n_jobs 32 --config ${tempFi.toString}").!(ProcessLogger(println, System.err.println))
+        if (exitValue < 0) {
+          throw new RuntimeException(f"error: {exitValue}")
+        } 
+      } finally {
+        Files.delete(tempFi)
+        new Directory(tempDir.toFile).deleteRecursively()
+      }
+    }
+  }
+
+}
---
+++ spark/src/main/scala/datatrans/step/PreprocSystem.scala
@@ -0,0 +1,49 @@
+package datatrans.step
+
+import sys.process.{Process, ProcessLogger}
+import sys.process._
+import java.io.File
+import java.io.PrintWriter
+import java.nio.file.Files
+import scala.reflect.io.Directory
+import org.apache.spark.sql.SparkSession
+import scopt._
+import io.circe.{Decoder}
+import io.circe.generic.semiauto._
+import datatrans.Utils._
+import datatrans.Config._
+import datatrans.Implicits._
+import datatrans._
+
+case class PreprocSystemConfig(
+  pyexec: String,
+  requirements: Seq[String],
+  workdir : String,
+  command : Seq[String]
+)
+
+object PreprocSystem extends StepImpl {
+
+  type ConfigType = PreprocSystemConfig
+
+  val configDecoder : Decoder[ConfigType] = deriveDecoder
+
+  def step(spark: SparkSession, config: PreprocSystemConfig): Unit = {
+
+    time {
+      val tempDir = Files.createTempDirectory(null)
+      try {
+        f"virtualenv -p ${config.pyexec} ${tempDir.toString}/venv".!
+        Process(Seq(f"${tempDir.toString}/venv/bin/pip", "install") ++ config.requirements).!
+        val exitValue = Process(f"${tempDir.toString}/venv/bin/python" +: config.command, new File(config.workdir)).!(ProcessLogger(println, println))
+        println(f"exitValue = ${exitValue}")
+        if (exitValue != 0) {
+          throw new RuntimeException(f"error: ${exitValue}")
+        }
+      } finally {
+        new Directory(tempDir.toFile).deleteRecursively()
+      }
+    }
+  }
+
+}
