ee971de32509f640c945f2738665412587acfd91
hyi <hongyi@renci.org>
2022-09-19 21:35:30 -0400

    remove code to handle non-numerical variables

--- spark/src/main/python/preprocPatient.py
+++ spark/src/main/python/preprocPatient.py
@@ -13,11 +13,7 @@ def preproc_patient(input_conf, input_file, output_file, pat_idx_df):
     df = pd.read_csv(input_file, quotechar='"')
 
     patient_cols = set(patient_cols) & set(df.columns)
-    
-    # filter out non-number columns
-    cols_type = df.dtypes
-    patient_cols = [col for col in patient_cols if cols_type[col] == np.float or cols_type[col] == np.int]
-    patient_cols.remove("AgeStudyStart")
+
     bins = []                                                                                                                                                         
                                                                                                                                                                       
     bins += preprocAge(df, "AgeStudyStart")                                                                                                                           
@@ -29,10 +25,7 @@ def preproc_patient(input_conf, input_file, output_file, pat_idx_df):
     addSex2(df)
 
     for c in patient_cols:
-        try:
-            df[c].fillna(0, inplace=True)
-        except ValueError:
-            print(f'ValueError: {c}')
+        df[c].fillna(0, inplace=True)
         df[c] = cut_col(df[c])
 
     df.drop(["birth_date"], axis=1, inplace=True)
